8b3bad283a051825d59dfaf071804687871906e8
João Ribeiro <ribeirojoaodasilva16@gmail.com>
2026-03-03 22:31:34 -0300

    Criando Personagens

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Aula03RH.Models;
+using Aula03RH.Models.Enuns;
+
+namespace Aula03RH
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            func.Id = 10;
+            func.Nome = "Neymar";
+            func.Cpf = "12345678910";
+            func.DataAdmissao = DateTime.Parse("01/01/2010");
+            func.Salario = 1000.00M;
+            func.TipoFuncionario = Models.Enuns.TipoFuncionarioEnum.CLT;
+
+            string mensagem = func.ExibirPeriodoExperiencia();
+            Console.WriteLine(mensagem);
+
+
+        }
+
+
+    }
+ 
+ 
+ }
+
